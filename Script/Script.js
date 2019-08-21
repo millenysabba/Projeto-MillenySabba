13f2b011bcb436c50dce4654a764da483d01ea7b
millenysabba <50713768+millenysabba@users.noreply.github.com>
2019-08-20 21:58:44 -0300

    Add files via upload

--- Script/Script.js
+++ Script/Script.js
@@ -5,46 +5,46 @@ $(document).ready(function(){
 Comandos que fazem os elemntos que fazem ter animação de subir e descer
 */
 
- // $("#flip").click(function(){
-  //  $("#panel").slideDown("fast");
- // });
+  $("#flip").click(function(){
+    $("#panel").slideDown("fast");
+  });
 
-//$("#flip2").click(function(){
-//    $("#panel").slideUp("1000");
- // });
+$("#flip2").click(function(){
+    $("#panel").slideUp("1000");
+  });
 
 /*
 animação que faz as duas animações anteriores
 */
-$("#Menu").click(function(){
-    $("#Principal").slideToggle("slow");
+$("#flip3").click(function(){
+    $("#panel").slideToggle("slow");
   });
 
 
 /////////////////FadeOut//////////////
 
-	//  $("#b1").click(function(){
-	   // $("#div1").fadeOut();
-	   // $("#div2").fadeOut("slow");
-	   // $("#div3").fadeOut(3000);
-	//  });
+	  $("#b1").click(function(){
+	    $("#div1").fadeOut();
+	    $("#div2").fadeOut("slow");
+	    $("#div3").fadeOut(3000);
+	  });
 
-	 // $("#b2").click(function(){
-	  //  $("#div1").fadeIn();
-	   // $("#div2").fadeIn("slow");
-	  //  $("#div3").fadeIn(3000);
-	 // });
+	  $("#b2").click(function(){
+	    $("#div1").fadeIn();
+	    $("#div2").fadeIn("slow");
+	    $("#div3").fadeIn(3000);
+	  });
 
 
 
 ///////////Append////////////
-// $("#btn1").click(function(){
-   // $("p").remove(" <b>Appended text</b>.");
- // });
+  $("#btn1").click(function(){
+    $("p").remove(" <b>Appended text</b>.");
+  });
 
-  //$("#btn2").click(function(){
-   // $("ol").remove("<li>Appended item</li>");
- //});
+  $("#btn2").click(function(){
+    $("ol").remove("<li>Appended item</li>");
+  });
 
 
 
